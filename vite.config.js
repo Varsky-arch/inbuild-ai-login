e924c6260ca78b117e77979aa9b9b9b2090ed3d4
Varsky-arch <akiozha@gmail.com>
2025-06-20 07:41:41 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Ganti dengan host Replit-mu (copas dari pesan error)
+const allowedReplitHost =
+  "62ca7113-b9c3-48f5-b471-4fd1ed647898-00-3nn4ixcjium7y.sisko.replit.dev";
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    strictPort: false,
-    allowedHosts: "all",
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [allowedReplitHost],
   },
 });
